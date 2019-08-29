6c3c73dab34178a7d725c1a1cb810cc7e0a29ec1
PraveenKumarDN <praveen.dn023@gmail.com>
2019-08-29 19:20:08 +0530

    Update Dummy.java
    changed the jwt value

--- src/test/java/Dummy.java
+++ src/test/java/Dummy.java
@@ -10,11 +10,11 @@ public class Dummy {
 
         List<String> list = new ArrayList<>();
         list.add("GKR5833861");
-        list.add("ELECTION_CARD");
+        list.add("asdfghj");
         boolean x = list.contains("GKR5833861"); // true
         boolean y = list.contains("foo"); // false
 
-        /*String jwt = "eyJvd25lciI6ImltcyIsImFsZyI6IlJTMjU2In0.eyJhdWQiOiIvcmVzdC9mY3dhbGxldC92MS9nZW5lcmF0ZU90cEZvckFkZEJhbmtEZXRhaWxzIiwidXNlcklkIjoiVmpBeEl6RXlNRGswTVRKbUxUQTJNVFV0TkdOaE9TMWhZbU16TFdKaVpHWTJOVFZtT1RSaE9BIiwiZmNVc2VySWQiOjEzMjUyMTY0MCwidmFsaWRDU1JGIjpmYWxzZSwiZmlyc3ROYW1lIjoiTWFyc2hhaW1zIiwibGFzdE5hbWUiOiJNYXJzaGFpbXMiLCJlbWFpbCI6Imltc3Rlc3RydmhmaS42NzcwNjQ5MDU1QG50bHdvcmxkLmluIiwibW9iaWxlIjoiNjQ2NzI0NDU4NiIsImRpc3BsYXluYW1lIjoic2FjcmlmaWNlIiwiZXhwIjoxNTQ5MDAxMTYwfQ.dycHhCnx1PAz_sybeEAyDnjri92dT-F0I4zclnXif5qIoB-y7Od57sY8zuexzt4qdJDdpd3oPvVLxsVZy8QVNgNVjM50kGVTzQ14kTTPaZJo3-w_h-Oi1rkgPHYfGs8sqL9rOPiZV73xLPUiC-tOG2dc5alo-W9465scLKvzdKrFrMdVx3roqgCaT6avf7FFGkKcwiSaSs6rXxL5NNQEAk5NVcx6hw3CdvE4jkRAaGEn_uQtNVCOoaoxv67SoYG1cNQ7lonvOOUuO3xveDAs5hRZhl8XYPlfr7Iim83q2pcbMgN3FLarTb4_0ivnFohG9QW4uscA1Wm66V_n7c3lrg" ;
+        /*String jwt = "rfyf" ;
 
         System.out.println("------------ Decode JWT ------------");
         String[] split_string = jwt.split("\\.");
